9bc06ea64edede6f68aea950f54084b516ee4233
patrikavaz <125215550+patrikavaz@users.noreply.github.com>
2025-11-06 12:39:17 +0000

    Liara.ir JS updated $(date +'%Y-%m-%d %H:%M')

---
+++ chunks/_next/static/EfoEtORNDeUDRAo0wYvn8/_buildManifest.js
@@ -0,0 +1,58 @@
+self.__BUILD_MANIFEST = function(s, c, a, t, e, u, g, p, o, i, l, d, r, b, n, h, f, k, j) {
+    return {
+        __rewrites: {
+            afterFiles: [],
+            beforeFiles: [],
+            fallback: []
+        },
+        __routerFilterStatic: {
+            numItems: 0,
+            errorRate: 1e-4,
+            numBits: 0,
+            numHashes: null,
+            bitArray: []
+        },
+        __routerFilterDynamic: {
+            numItems: 0,
+            errorRate: 1e-4,
+            numBits: o,
+            numHashes: null,
+            bitArray: []
+        },
+        "/": [s, c, a, t, "static/chunks/pages/index-2a439100fa91dd37.js"],
+        "/404": [s, c, a, t, "static/chunks/pages/404-4b14f210c6a1f2d5.js"],
+        "/_error": ["static/chunks/pages/_error-69472128dcb42fe2.js"],
+        "/about": [s, c, a, t, "static/chunks/pages/about-52fb4537a40e7e59.js"],
+        "/abuse": [s, c, a, t, "static/chunks/pages/abuse-8127941391b2603e.js"],
+        "/blog": [s, c, a, g, t, "static/chunks/pages/blog-39a1e406f5aef0eb.js"],
+        "/blog/author/[slug]": [s, c, a, g, t, "static/chunks/pages/blog/author/[slug]-d3a9153cb54a0af8.js"],
+        "/blog/author/[slug]/page/[page]": [s, c, a, g, t, "static/chunks/pages/blog/author/[slug]/page/[page]-75926bacaf607320.js"],
+        "/blog/category/[slug]": [s, c, a, g, t, "static/chunks/pages/blog/category/[slug]-9fc457c7ebc45608.js"],
+        "/blog/category/[slug]/page/[page]": [s, c, a, g, t, "static/chunks/pages/blog/category/[slug]/page/[page]-23f522d1f3e37e96.js"],
+        "/blog/page/[slug]": [s, c, a, g, t, "static/chunks/pages/blog/page/[slug]-3b3543a89a2d86de.js"],
+        "/blog/tag/[slug]": [s, c, a, g, t, "static/chunks/pages/blog/tag/[slug]-2ef7697257587fe8.js"],
+        "/blog/tag/[slug]/page/[page]": [s, c, a, g, t, "static/chunks/pages/blog/tag/[slug]/page/[page]-2f28cda0d2ebaa2c.js"],
+        "/blog/tags": [s, c, a, t, "static/chunks/pages/blog/tags-64d155e136720c99.js"],
+        "/blog/[slug]": [s, "static/chunks/fea29d9f-4f48e549ee05e8a6.js", "static/chunks/41155975-15f2d3811d324391.js", "static/chunks/cb355538-3794f969d72c43f2.js", c, "static/chunks/9878-bb1d03da4114573e.js", a, t, "static/chunks/pages/blog/[slug]-44ddd09003e05cab.js"],
+        "/bug-bounty": [s, c, a, t, "static/chunks/pages/bug-bounty-3afcaa9192a33d1c.js"],
+        "/landing/[slug]": [s, c, a, e, u, p, t, "static/chunks/pages/landing/[slug]-a277b5ab9072e31a.js"],
+        "/one-click-apps": [s, c, r, a, t, "static/chunks/pages/one-click-apps-4af24376a40b64af.js"],
+        "/one-click-apps/[slug]": [s, c, r, a, e, u, "static/css/9f0db14418d5753d.css", "static/chunks/pages/one-click-apps/[slug]-2fbd4c337c00ee0b.js"],
+        "/pricing": [s, b, c, n, a, e, u, i, h, "static/css/4e480904913c580b.css", "static/chunks/pages/pricing-19ba2d2f8acc6738.js"],
+        "/privacy-policy": [s, c, a, t, "static/chunks/pages/privacy-policy-bb4c5ad44c1172c4.js"],
+        "/products/ai": [s, c, a, e, t, "static/chunks/pages/products/ai-6e83bcf52f31a7b9.js"],
+        "/products/ai/[slug]": [s, c, a, t, "static/chunks/pages/products/ai/[slug]-d18e0d00bb828d52.js"],
+        "/products/cloud-server": [s, c, a, e, u, p, t, "static/chunks/pages/products/cloud-server-1a48510ba8687beb.js"],
+        "/products/dns": [s, c, f, a, p, t, "static/chunks/pages/products/dns-5779d64fd93d562e.js"],
+        "/products/email": [s, c, k, a, p, i, j, "static/css/0bfb907f9787ae47.css", "static/chunks/pages/products/email-f49f5d7f23d470a6.js"],
+        "/products/free-cloud-host": [s, c, a, e, u, t, "static/chunks/pages/products/free-cloud-host-8d8b9462989a3332.js"],
+        "/products/gaming-vps": [s, c, a, e, u, t, "static/chunks/pages/products/gaming-vps-85fcd736c9382ed1.js"],
+        "/products/iranian-vps": [s, c, a, e, u, t, "static/chunks/pages/products/iranian-vps-b5ae1562c76e95ba.js"],
+        "/products/object-storage": [s, b, c, k, n, f, a, e, u, p, i, j, h, "static/css/e333a54558801f89.css", "static/chunks/pages/products/object-storage-2451bc5e749f2126.js"],
+        "/products/vps": [s, c, a, e, u, t, "static/chunks/pages/products/vps-819ff7af6209d9b6.js"],
+        "/products/[slug]": [s, c, a, e, u, i, t, "static/chunks/pages/products/[slug]-70aca50a5d532875.js"],
+        "/sla": [s, c, a, t, "static/chunks/pages/sla-ad6e2ab3096ae0ab.js"],
+        "/terms": [s, c, a, t, "static/chunks/pages/terms-2e8f2348beaa6c5c.js"],
+        sortedPages: ["/", "/404", "/_app", "/_error", "/about", "/abuse", "/blog", "/blog/author/[slug]", "/blog/author/[slug]/page/[page]", "/blog/category/[slug]", "/blog/category/[slug]/page/[page]", "/blog/page/[slug]", "/blog/tag/[slug]", "/blog/tag/[slug]/page/[page]", "/blog/tags", "/blog/[slug]", "/bug-bounty", "/landing/[slug]", "/one-click-apps", "/one-click-apps/[slug]", "/pricing", "/privacy-policy", "/products/ai", "/products/ai/[slug]", "/products/cloud-server", "/products/dns", "/products/email", "/products/free-cloud-host", "/products/gaming-vps", "/products/iranian-vps", "/products/object-storage", "/products/vps", "/products/[slug]", "/sla", "/terms"]
+    }
+}("static/chunks/ebc70433-8d1719ac23e3de00.js", "static/chunks/2835-0ff218658e2ea96d.js", "static/chunks/8848-1268d9c2a1b30f82.js", "static/css/dfc228b36acf10d2.css", "static/chunks/1729-4ffb64fff7a4c54e.js", "static/chunks/3897-acff2702b619e6a6.js", "static/chunks/4028-906678e4f3d00182.js", "static/chunks/6495-e6ce0dabadad680e.js", 0, "static/chunks/3688-3f3291dde121b60c.js", 0, 0, "static/chunks/3951-89055170527de682.js", "static/chunks/5727625e-a95c7cd12ec7ab20.js", "static/chunks/8577-6ce51d5f1828c5db.js", "static/chunks/5541-14088a5f3ebce1b2.js", "static/chunks/5605-1304612e346a884b.js", "static/chunks/2715-64de27190afa5181.js", "static/chunks/4674-7ed7068866c3a184.js"), self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
